590c2632b791b5014ec29e7c9aceac0472a6f5ad
lahaina <nattolecats@gmail.com>
2023-09-12 16:08:51 -0500

    sm6375-common: [TMP] Switch back to bash scripts for now * Until python scripts stabilize for our usage

---
+++ extract-files.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=sm6375-common
+VENDOR=motorola
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+ONLY_FIRMWARE=
+KANG=
+SECTION=
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        --only-firmware)
+            ONLY_FIRMWARE=true
+            ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
+    esac
+    shift
+done
+
+if [ -z "${SRC}" ]; then
+    SRC="adb"
+fi
+
+function blob_fixup() {
+    case "${1}" in
+        system_ext/lib64/libwfdnative.so)
+            [ "$2" = "" ] && return 0
+            ${PATCHELF} --remove-needed "android.hidl.base@1.0.so" "${2}"
+            ;;
+        system_ext/etc/permissions/moto-telephony.xml)
+            [ "$2" = "" ] && return 0
+            sed -i "s#/system/#/system_ext/#" "${2}"
+            ;;
+        vendor/etc/vintf/manifest/vendor.dolby.media.c2@1.0-service.xml)
+            [ "$2" = "" ] && return 0
+            sed -ni '/default9/!p' "${2}"
+            ;;
+        vendor/etc/qcril_database/qcrilNr.db|\
+        vendor/etc/qcril_database/upgrade/config/*)
+            [ "$2" = "" ] && return 0
+            sed -i '/persist.vendor.radio.poweron_opt/ s/1/0/g' "${2}"
+            ;;
+        vendor/lib64/libwvhidl.so)
+            [ "$2" = "" ] && return 0
+            grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+if [ -z "${ONLY_FIRMWARE}" ]; then
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
+
+#if [ -z "${SECTION}" ]; then
+#    extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
+#fi
+
+"${MY_DIR}/setup-makefiles.sh"
